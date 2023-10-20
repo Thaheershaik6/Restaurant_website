1c5a995b2ea881f5e087c1653e5bd02acd4c06d8
Thaheershaik6 <thaheershaik866@gmail.com>
2023-10-20 21:47:33 +0530

    Cart open and close updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,21 +5,23 @@ import Modal from "../UI/Modal";
 
 const Cart = (props) => {
   const cartitems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", amount: 2, prise: 22.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
   return (
-    <Modal>
+    <Modal onClose={props.onCloseCart}>
       {cartitems}
       <div className={classes.total}>
         <span>Total Amont</span>
         <span>35.78</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import mealsImage from '../assets/Header-image.jpg';
+import mealsImage from "../assets/Header-image.jpg";
 
-import classes from './Header.module.css';
-import HeaderCartButton from './HeaderCratButton';
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCratButton";
 
 const Header = (props) => {
-
-    return(
-        <div>
-            <header className={classes.header}>
-                <h2>React Meals</h2>
-                <HeaderCartButton></HeaderCartButton>
-                </header>
-            <div className={classes['main-image']}>
-            <img src={mealsImage} alt='A table of full delicious food!'/>
-            </div>            
-        </div>
-    );
-}
+  return (
+    <div>
+      <header className={classes.header}>
+        <h2>React Meals</h2>
+        <HeaderCartButton onClick={props.onShowCart}></HeaderCartButton>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table of full delicious food!" />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
