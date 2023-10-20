29ac31e02fd593d1bc6bda1d26df4b928a72fedb
Thaheershaik6 <thaheershaik866@gmail.com>
2023-10-20 18:51:07 +0530

    Cart without any Functionality updated

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+
+const Cart = (props) => {
+  const cartitems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: "c1", name: "Sushi", amount: 2, prise: 22.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Modal>
+      {cartitems}
+      <div className={classes.total}>
+        <span>Total Amont</span>
+        <span>35.78</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
